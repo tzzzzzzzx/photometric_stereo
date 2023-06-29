5442b44900b71937fd4e7ff5b29afdd1197b5fb3
tzzzzzzzx <414850847@qq.com>
2023-06-29 09:45:52 +0800

    managed to rewrite algorithm of heightmap calc

--- heightMap.py
+++ heightMap.py
@@ -2,101 +2,66 @@ import numpy as np
 import scipy
 import cv2
 
-def compute_depth(mask,N):
+
+def compute_depth(mask, N):
     """
     compute the depth picture
     """
-    im_h, im_w = mask.shape
-    N = np.reshape(N, (im_h, im_w, 3))
+    M = scipy.sparse.lil_matrix((mask.size * 2, mask.size))
+    v = np.zeros(M.shape[0], dtype=np.float32)
 
-    # =================get the non-zero index of mask=================
+    nx = N[:, :, 0].ravel()
+    ny = N[:, :, 1].ravel()
+    nz = N[:, :, 2].ravel()
+
+    v[0:nx.shape[0]] = -nx / (nz + 1e-8)
+    v[nx.shape[0]:v.shape[0]] = -ny / (nz + 1e-8)
+
     obj_h, obj_w = np.where(mask != 0)
-    no_pix = np.size(obj_h) #37244
-    #print(no_pix)
-    full2obj = np.zeros((im_h, im_w))
-    for idx in range(np.size(obj_h)):
-        full2obj[obj_h[idx], obj_w[idx]] = idx
-    full2obj = np.round(full2obj).astype(int)
-    M = scipy.sparse.lil_matrix((2*no_pix, no_pix))
-    v = np.zeros((2*no_pix, 1))
+    # 得到非零元素的数量
+    no_pix = np.size(obj_h)
 
-    # ================= fill the M&V =================
-    for idx in range(no_pix):
-        # obtain the 2D coordinate
-        h = obj_h[idx]
-        w = obj_w[idx]
-        # obtian the surface normal vector
-        if 0 < h < im_h-1 and 0 < w < im_w-1:
-            n_x = N[h, w, 0]
-            n_y = N[h, w, 1]
-            n_z = N[h, w, 2]
-            
-            row_idx = idx * 2
-            if mask[h, w+1]:
-                idx_horiz = full2obj[h, w+1]
-                M[row_idx, idx] = -1
-                M[row_idx, idx_horiz] = 1
-                if n_z==0:
-                    v[row_idx] = 0
-                else:
-                    v[row_idx] = -n_x / n_z
-            elif mask[h, w-1]:
-                idx_horiz = full2obj[h, w-1]
-                M[row_idx, idx_horiz] = -1
-                M[row_idx, idx] = 1
-                if n_z==0:
-                    v[row_idx] = 0
-                else:
-                    v[row_idx] = -n_x / n_z
-    
-            row_idx = idx * 2 + 1
-            if mask[h+1, w]:
-                idx_vert = full2obj[h+1, w]
-                M[row_idx, idx] = 1
-                M[row_idx, idx_vert] = -1
-                if n_z==0:
-                    v[row_idx] = 0
-                else:
-                    v[row_idx] = -n_y / n_z
-            elif mask[h-1, w]:
-                idx_vert = full2obj[h-1, w]
-                M[row_idx, idx_vert] = 1
-                M[row_idx, idx] = -1
-                if n_z==0:
-                    v[row_idx] = 0
-                else:
-                    v[row_idx] = -n_y / n_z
-    
+    numPixels = mask.size
+    width = mask.shape[1]
+    height = mask.shape[0]
 
-    # =================sloving the linear equations Mz = v=================
+    for i in range(height):
+        for j in range(width):
+            pixel = width * i + j
+            if j != width - 1:
+                M[pixel, pixel] = -1
+                M[pixel, pixel + 1] = 1
+            if i != height - 1:
+                M[pixel + numPixels, pixel] = -1
+                M[pixel + numPixels, pixel + width] = 1
 
     MtM = M.T @ M
     Mtv = M.T @ v
-    for i in range(MtM.shape[0]):
-            MtM[i,i] = MtM[i,i]+1e-6
-    z = scipy.sparse.linalg.spsolve(MtM, Mtv)
-    #print(z)
+    z, _ = scipy.sparse.linalg.cg(MtM, Mtv)
     std_z = np.std(z, ddof=1)
     mean_z = np.mean(z)
     z_zscore = (z - mean_z) / std_z
+
+    # 因奇异值造成的异常
     outlier_ind = np.abs(z_zscore) > 10
     z_min = np.min(z[~outlier_ind])
     z_max = np.max(z[~outlier_ind])
 
     Z = mask.astype('float')
     for idx in range(no_pix):
-        # obtain the position in 2D picture 
-        h = obj_h[idx]
-        w = obj_w[idx]
-        Z[h, w] = (z[idx] - z_min) / (z_max - z_min) * 255
+        t_height = obj_h[idx]
+        t_width = obj_w[idx]
+        Z[t_height, t_width] = (z[t_height * width + t_width] -
+                                z_min) / (z_max - z_min) * 255
 
-    depth = Z
-    return depth
+    return Z
 
-def save_depthmap(depth,filename=None):
+
+def save_depthmap(depth, filename=None):
     if filename is None:
         raise ValueError("filename is None")
     np.save(filename, depth)
+
 
 def display_depthmap(depth=None, delay=0, name=None):
     depth = np.uint8(depth)
@@ -105,4 +70,3 @@ def display_depthmap(depth=None, delay=0, name=None):
     cv2.imshow(name, depth)
     cv2.waitKey()
     cv2.destroyAllWindows()
-
